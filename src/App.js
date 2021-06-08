3a4b953e8828dc82070fb7a65b0e3f97d78dcdcc
aprin418 <aprin418@gmail.com>
2021-06-08 14:13:03 -0400

    initial setup and authourization

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // Imports
-import React, { useEffect, useState } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
-// import setAuthToken from './utils/setAuthToken';
+import React, { useEffect, useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 // CSS
-import './App.css';
+import "./App.css";
 // Components
-import Welcome from './components/Welcome';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Profile from './components/Profile';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import About from './components/About';
+import Welcome from "./components/Welcome";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import About from "./components/About";
 
 function App() {
   // Set state values
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       {/* <Navbar  /> */}
-      
+
       <Footer />
     </div>
   );
